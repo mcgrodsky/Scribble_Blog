325d467ea5bf862444bd3e08cf97d1f9246b31a6
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-10 20:44:03 -0500

    did all the controllers for comments. need to work on views....

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,10 +14,44 @@ class CommentsController < ApplicationController
 
   #create
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
 
+    redirect_to post_comments_path(@post, @comment)
   end
 
+  #show
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
 
+  # edit
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment= Comment.find(params[:id])
+  end
 
+  # update
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
 
+    redirect_to post_comments_path(@post, @comment)
+  end
+
+  # destroy
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_comments_path( @post )
+  end
+
+private
+def comment_params
+  params.require(:comment).permit(:content)
+end
 end
